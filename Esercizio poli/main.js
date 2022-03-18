16a0ffda5b5b7a72bed5ae0f6d733a124777be4f
SimonePavone <simonebest56@gmail.com>
2022-03-18 17:48:55 +0100

    Esercizio poli terminato

--- Esercizio poli/main.js
+++ Esercizio poli/main.js
@@ -2,3 +2,19 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
+var parola = 'osso';
+
+var parolaInversa = prompt("Inserisci parola")
+
+//var parolaInversa = invertiParola(parola);
+
+if(parola == parolaInversa){
+    console.log('la parola è palindroma');
+  } else {
+    console.log('la parola non è palindroma');
+  }
+  
+function invertiParola(str){
+  var strInversa = str.split('').reverse().join('');  
+  return strInversa;
+}
